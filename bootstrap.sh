b4b6ce496e601cae9a860f40a90fa9b7c42d3d9e
Simon Ljungberg <hi@iamsim.me>
2017-09-11 22:11:34 +0200

    Add bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#
+# # Download dotfiles
+git clone git@github.com:simme/dotfiles.git ~/.dotfiles
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.config ~/.config
 ln -s ~/.dotfiles/.brewfile ~/.brewfile
@@ -16,4 +22,3 @@ git clone https://github.com/joedynamite/base16-xcode Base16
 ln -s Base16/* ./
 
 ./osx_preferences.sh
-
